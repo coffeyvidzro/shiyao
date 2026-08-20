65c4b81981df56a790976d26a543582e37b7d151
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-20 21:00:51 +0000

    gofmt guest resource limits

--- shiyao/cmd/guest-agent/limits.go
+++ shiyao/cmd/guest-agent/limits.go
@@ -22,13 +22,22 @@ const (
 )
 
 func startWithResourceLimits(cmd *exec.Cmd) error {
-	if cmd.SysProcAttr == nil { cmd.SysProcAttr = &syscall.SysProcAttr{} }
+	if cmd.SysProcAttr == nil {
+		cmd.SysProcAttr = &syscall.SysProcAttr{}
+	}
 	cmd.SysProcAttr.Setpgid = true
 	cmd.SysProcAttr.Pdeathsig = syscall.SIGKILL
-	if err := cmd.Start(); err != nil { return fmt.Errorf("start command: %w", err) }
+
+	if err := cmd.Start(); err != nil {
+		return fmt.Errorf("start command: %w", err)
+	}
 
 	pid := cmd.Process.Pid
-	killAndReap := func() { _ = unix.Kill(-pid, unix.SIGKILL); _ = cmd.Wait() }
+	killAndReap := func() {
+		_ = unix.Kill(-pid, unix.SIGKILL)
+		_ = cmd.Wait()
+	}
+
 	if err := applyRlimits(pid); err != nil {
 		killAndReap()
 		return fmt.Errorf("apply process limits: %w", err)
@@ -41,27 +50,37 @@ func startWithResourceLimits(cmd *exec.Cmd) error {
 }
 
 func applyRlimits(pid int) error {
-	limits := []struct{ resource int; value uint64 }{
+	limits := []struct {
+		resource int
+		value    uint64
+	}{
 		{unix.RLIMIT_CPU, 300},
 		{unix.RLIMIT_FSIZE, guestFileMax},
 		{unix.RLIMIT_NOFILE, guestNoFileMax},
 	}
-	for _, l := range limits {
-		r := &unix.Rlimit{Cur: l.value, Max: l.value}
-		if err := unix.Prlimit(pid, l.resource, r, nil); err != nil { return err }
+	for _, limit := range limits {
+		rlimit := &unix.Rlimit{Cur: limit.value, Max: limit.value}
+		if err := unix.Prlimit(pid, limit.resource, rlimit, nil); err != nil {
+			return err
+		}
 	}
 	return nil
 }
 
 func addToCgroup(pid int) error {
-	if err := os.MkdirAll(guestCgroupRoot, 0755); err != nil { return err }
+	if err := os.MkdirAll(guestCgroupRoot, 0755); err != nil {
+		return err
+	}
+
 	settings := map[string]string{
 		"memory.max": strconv.Itoa(guestMemoryMax),
 		"pids.max":   strconv.Itoa(guestPidsMax),
 		"cpu.max":    fmt.Sprintf("%d %d", guestCPUQuotaUS, guestCPUPeriodUS),
 	}
 	for name, value := range settings {
-		if err := os.WriteFile(filepath.Join(guestCgroupRoot, name), []byte(value), 0644); err != nil { return err }
+		if err := os.WriteFile(filepath.Join(guestCgroupRoot, name), []byte(value), 0644); err != nil {
+			return err
+		}
 	}
 	return os.WriteFile(filepath.Join(guestCgroupRoot, "cgroup.procs"), []byte(strconv.Itoa(pid)), 0644)
 }
