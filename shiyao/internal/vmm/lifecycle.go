ecf7bc52008df378c0a4e4c7f59762f747f216cf
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-20 21:12:18 +0000

    refactor: isolate VMM lifecycle state machine

---
+++ shiyao/internal/vmm/lifecycle.go
@@ -0,0 +1,26 @@
+package vmm
+
+type State uint8
+
+const (
+	StateCreated State = iota
+	StateConfiguring
+	StateConfigured
+	StateRunning
+	StateStopping
+	StateStopped
+	StateCleanupFailed
+)
+
+func (s State) String() string {
+	switch s {
+	case StateCreated: return "created"
+	case StateConfiguring: return "configuring"
+	case StateConfigured: return "configured"
+	case StateRunning: return "running"
+	case StateStopping: return "stopping"
+	case StateStopped: return "stopped"
+	case StateCleanupFailed: return "cleanup-failed"
+	default: return "unknown"
+	}
+}
