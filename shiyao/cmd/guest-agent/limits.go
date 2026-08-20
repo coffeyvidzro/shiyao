15b7978c686db5d4a1a19d9edfaf736135d741a1
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-20 21:01:28 +0000

    Place commands in cgroups atomically before exec

--- shiyao/cmd/guest-agent/limits.go
+++ shiyao/cmd/guest-agent/limits.go
@@ -22,11 +22,19 @@ const (
 )
 
 func startWithResourceLimits(cmd *exec.Cmd) error {
+	cgroup, err := prepareCgroup()
+	if err != nil {
+		return fmt.Errorf("prepare cgroup: %w", err)
+	}
+	defer func() { _ = cgroup.Close() }()
+
 	if cmd.SysProcAttr == nil {
 		cmd.SysProcAttr = &syscall.SysProcAttr{}
 	}
 	cmd.SysProcAttr.Setpgid = true
 	cmd.SysProcAttr.Pdeathsig = syscall.SIGKILL
+	cmd.SysProcAttr.UseCgroupFD = true
+	cmd.SysProcAttr.CgroupFD = int(cgroup.Fd())
 
 	if err := cmd.Start(); err != nil {
 		return fmt.Errorf("start command: %w", err)
@@ -42,11 +50,30 @@ func startWithResourceLimits(cmd *exec.Cmd) error {
 		killAndReap()
 		return fmt.Errorf("apply process limits: %w", err)
 	}
-	if err := addToCgroup(pid); err != nil {
-		killAndReap()
-		return fmt.Errorf("apply cgroup limits: %w", err)
-	}
 	return nil
+}
+
+func prepareCgroup() (*os.File, error) {
+	if err := os.MkdirAll(guestCgroupRoot, 0755); err != nil {
+		return nil, err
+	}
+
+	settings := map[string]string{
+		"memory.max": strconv.Itoa(guestMemoryMax),
+		"pids.max":   strconv.Itoa(guestPidsMax),
+		"cpu.max":    fmt.Sprintf("%d %d", guestCPUQuotaUS, guestCPUPeriodUS),
+	}
+	for name, value := range settings {
+		if err := os.WriteFile(filepath.Join(guestCgroupRoot, name), []byte(value), 0644); err != nil {
+			return nil, err
+		}
+	}
+
+	fd, err := unix.Open(guestCgroupRoot, unix.O_PATH|unix.O_DIRECTORY|unix.O_CLOEXEC, 0)
+	if err != nil {
+		return nil, err
+	}
+	return os.NewFile(uintptr(fd), guestCgroupRoot), nil
 }
 
 func applyRlimits(pid int) error {
@@ -65,22 +92,4 @@ func applyRlimits(pid int) error {
 		}
 	}
 	return nil
-}
-
-func addToCgroup(pid int) error {
-	if err := os.MkdirAll(guestCgroupRoot, 0755); err != nil {
-		return err
-	}
-
-	settings := map[string]string{
-		"memory.max": strconv.Itoa(guestMemoryMax),
-		"pids.max":   strconv.Itoa(guestPidsMax),
-		"cpu.max":    fmt.Sprintf("%d %d", guestCPUQuotaUS, guestCPUPeriodUS),
-	}
-	for name, value := range settings {
-		if err := os.WriteFile(filepath.Join(guestCgroupRoot, name), []byte(value), 0644); err != nil {
-			return err
-		}
-	}
-	return os.WriteFile(filepath.Join(guestCgroupRoot, "cgroup.procs"), []byte(strconv.Itoa(pid)), 0644)
 }
