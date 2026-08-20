f38e85f8ca785c818c7b36570b1b6aa763d95c11
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-20 21:24:25 +0000

    gofmt vmm lifecycle

--- shiyao/internal/vmm/lifecycle.go
+++ shiyao/internal/vmm/lifecycle.go
@@ -14,13 +14,21 @@ const (
 
 func (s State) String() string {
 	switch s {
-	case StateCreated: return "created"
-	case StateConfiguring: return "configuring"
-	case StateConfigured: return "configured"
-	case StateRunning: return "running"
-	case StateStopping: return "stopping"
-	case StateStopped: return "stopped"
-	case StateCleanupFailed: return "cleanup-failed"
-	default: return "unknown"
+	case StateCreated:
+		return "created"
+	case StateConfiguring:
+		return "configuring"
+	case StateConfigured:
+		return "configured"
+	case StateRunning:
+		return "running"
+	case StateStopping:
+		return "stopping"
+	case StateStopped:
+		return "stopped"
+	case StateCleanupFailed:
+		return "cleanup-failed"
+	default:
+		return "unknown"
 	}
 }
