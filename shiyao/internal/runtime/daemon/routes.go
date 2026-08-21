1082829325f6e0ebe1786a7565cd24449a4bc4bf
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-21 19:21:04 +0000

    feat(runtime): apply authentication middleware

--- shiyao/internal/runtime/daemon/routes.go
+++ shiyao/internal/runtime/daemon/routes.go
@@ -3,12 +3,14 @@ package daemon
 import (
 	"net/http"
 
+	"github.com/gin-gonic/gin"
+
 	"github.com/coffeyvidzro/shiyao/internal/identity/auth"
+	"github.com/coffeyvidzro/shiyao/internal/identity/pat"
 	"github.com/coffeyvidzro/shiyao/internal/identity/session"
 	"github.com/coffeyvidzro/shiyao/internal/identity/users"
 	"github.com/coffeyvidzro/shiyao/internal/platform/sandbox"
-
-	"github.com/gin-gonic/gin"
+	"github.com/coffeyvidzro/shiyao/internal/runtime/middleware"
 )
 
 func RegisterRoutes(router *gin.Engine, modules Modules) {
@@ -16,9 +18,15 @@ func RegisterRoutes(router *gin.Engine, modules Modules) {
 		c.Status(http.StatusNoContent)
 	})
 
+	authMiddleware := middleware.NewAuth(
+		modules.Session.Service,
+		modules.PAT.Service,
+	).Handler()
+
 	api := router.Group("/v1")
-	auth.RegisterRoutes(api, modules.Auth.Handler)
-	session.RegisterRoutes(api, modules.Session.Handler)
-	users.RegisterRoutes(api, modules.Users.Handler)
-	sandbox.RegisterRoutes(api, modules.Sandbox.Handler)
+	auth.RegisterRoutes(api, modules.Auth.Handler, authMiddleware)
+	pat.RegisterRoutes(api, modules.PAT.Handler, authMiddleware)
+	session.RegisterRoutes(api, modules.Session.Handler, authMiddleware)
+	users.RegisterRoutes(api, modules.Users.Handler, authMiddleware)
+	sandbox.RegisterRoutes(api, modules.Sandbox.Handler, authMiddleware)
 }
