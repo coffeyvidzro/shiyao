c85525d2e16a50ef35c4382ecf0f33b335896718
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-21 19:13:50 +0000

    feat(runtime): register PAT module

--- shiyao/internal/runtime/daemon/modules.go
+++ shiyao/internal/runtime/daemon/modules.go
@@ -2,6 +2,7 @@ package daemon
 
 import (
 	"github.com/coffeyvidzro/shiyao/internal/identity/auth"
+	"github.com/coffeyvidzro/shiyao/internal/identity/pat"
 	"github.com/coffeyvidzro/shiyao/internal/identity/session"
 	"github.com/coffeyvidzro/shiyao/internal/identity/users"
 	"github.com/coffeyvidzro/shiyao/internal/platform/sandbox"
@@ -9,6 +10,7 @@ import (
 
 type Modules struct {
 	Auth    AuthModule
+	PAT     PATModule
 	Session SessionModule
 	Users   UserModule
 	Sandbox SandboxModule
@@ -18,6 +20,12 @@ type AuthModule struct {
 	Repository *auth.Repository
 	Service    *auth.Service
 	Handler    *auth.Handler
+}
+
+type PATModule struct {
+	Repository *pat.Repository
+	Service    *pat.Service
+	Handler    *pat.Handler
 }
 
 type SessionModule struct {
