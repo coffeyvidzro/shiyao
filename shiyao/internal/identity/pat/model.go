2896fc5a7795580fa7bdae811dde9efcbfcdb1af
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-21 19:08:20 +0000

    feat(pat): add token models

---
+++ shiyao/internal/identity/pat/model.go
@@ -0,0 +1,40 @@
+package pat
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Token struct {
+	ID         uuid.UUID
+	UserID     uuid.UUID
+	Name       string
+	TokenHash  string
+	Prefix     string
+	Scopes     []string
+	ExpiresAt  *time.Time
+	LastUsedAt *time.Time
+	CreatedAt  time.Time
+}
+
+type CreateRequest struct {
+	Name      string     `json:"name"`
+	Scopes    []string   `json:"scopes"`
+	ExpiresAt *time.Time `json:"expires_at"`
+}
+
+type Response struct {
+	ID         uuid.UUID  `json:"id"`
+	Name       string    `json:"name"`
+	Prefix     string    `json:"prefix"`
+	Scopes     []string  `json:"scopes"`
+	ExpiresAt  *time.Time `json:"expires_at,omitempty"`
+	LastUsedAt *time.Time `json:"last_used_at,omitempty"`
+	CreatedAt  time.Time  `json:"created_at"`
+}
+
+type CreateResponse struct {
+	Response
+	Token string `json:"token"`
+}
