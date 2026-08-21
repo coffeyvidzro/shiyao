16ef7dcd3a479a3fa1bf67d4b12a433f9c10ed1f
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-21 05:33:13 +0000

    test(network): reject IPv6 configuration

---
+++ shiyao/internal/network/config_test.go
@@ -0,0 +1,30 @@
+package network
+
+import "testing"
+
+func TestConfigValidateRejectsIPv6Host(t *testing.T) {
+	cfg := DefaultConfig("tap0")
+	cfg.HostIP = "fd00::1"
+
+	if err := cfg.Validate(); err == nil {
+		t.Fatal("expected IPv6 host IP to be rejected")
+	}
+}
+
+func TestConfigValidateRejectsIPv6Guest(t *testing.T) {
+	cfg := DefaultConfig("tap0")
+	cfg.HostIP = "fd00::1"
+	cfg.GuestIP = "fd00::2/64"
+
+	if err := cfg.Validate(); err == nil {
+		t.Fatal("expected IPv6 guest IP to be rejected")
+	}
+}
+
+func TestConfigValidateAcceptsIPv4(t *testing.T) {
+	cfg := DefaultConfig("tap0")
+
+	if err := cfg.Validate(); err != nil {
+		t.Fatalf("expected default IPv4 configuration to validate: %v", err)
+	}
+}
