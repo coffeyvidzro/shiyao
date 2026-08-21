cd9b84ce195ea4744a0331858422d9d8be783355
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-21 19:09:51 +0000

    fix(pat): update token usage timestamp

--- shiyao/internal/database/queries/personal_access_tokens.sql
+++ shiyao/internal/database/queries/personal_access_tokens.sql
@@ -36,5 +36,5 @@ WHERE id = sqlc.arg(id)
 -- name: TouchToken :exec
 UPDATE personal_access_tokens
 SET last_used_at = NOW()
-WHERE id = sqlc.arg(id)
-  AND last_used_at < NOW() - INTERVAL '1 minute';
+WHERE last_used_at IS NULL
+   OR last_used_at < NOW() - INTERVAL '1 minute';
