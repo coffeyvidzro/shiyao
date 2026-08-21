b171e85389097de0c8b51a51ea32ee9bc3d604db
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-21 18:17:33 +0000

    feat(authn): define credential types

--- shiyao/internal/authn/credential.go
+++ shiyao/internal/authn/credential.go
@@ -1,1 +1,20 @@
 package authn
+
+import "github.com/google/uuid"
+
+type CredentialType string
+
+const (
+	CredentialSession CredentialType = "session"
+	CredentialPAT     CredentialType = "pat"
+)
+
+type Credential struct {
+	ID   uuid.UUID
+	Type CredentialType
+}
+
+type CredentialInput struct {
+	Type  CredentialType
+	Value string
+}
