68e77d0a0260d0f1cf4a99df7ce8f83b1ea1a84f
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-22 15:37:22 +0000

    refactor: add Polar license validation client operation

--- internal/adapters/polar/client.go
+++ internal/adapters/polar/client.go
@@ -5,4 +5,5 @@ import "context"
 // Client provides access to the Polar API used by Shiyao.
 type Client interface {
 	GetLicenseKey(context.Context, string) (*LicenseKey, error)
+	ValidateLicenseKey(context.Context, ValidateLicenseKeyRequest) (*ValidatedLicenseKey, error)
 }
